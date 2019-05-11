dbf09d02b29d3a0a63b6e515d45fdbf6f20c13b7
Andry <andryj9622@gmail.com>
2019-05-11 19:07:59 -0400

    Used GEN_REGISTER for this block

--- Files/ADDR_AND_DATA_REG.vhd
+++ Files/ADDR_AND_DATA_REG.vhd
@@ -1,15 +1,15 @@
 ----------------------------------------------------------------------------------
 -- MIT License
 --	Copyright (c) 2019
--- Engineers: Andry J. Hernández Rodríguez & Dariel Suárez González 
+-- Engineers: Andry J. Hernandez Rodriguez 
 -- 
--- Create Date:    22:33:33 06/05/2019 
+-- Create Date:    22:33:33 05/06/2019 
 -- Design Name: 	 8254_Timer
 -- Module Name:    ADDR_DATA_REG 
 -- Project Name:   FPGA_8254_Timer
 -- Target Devices: Spaartan3E-XC3S1600E
 -- Tool versions:  Xilinx ISE 14.7
--- Description: 	 Timer 8254 - 7 chanels with modes 1 and 3
+-- Description: 	 Save data bus and address bus when WR
 --
 
 ----------------------------------------------------------------------------------
@@ -18,37 +18,72 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ADDR_DATA_REG is
 	
+	Generic( lenght_data : integer := 8;
+				lenght_addr : integer := 3);
     Port (  
-           
-		  CLK,CS,RST : IN STD_LOGIC;
-		  A:IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-		  DIN:IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		  AP:OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-		  DI:OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
+		  CLK,CS,RST,RD,WR : IN STD_LOGIC;
+		  A:IN STD_LOGIC_VECTOR (lenght_addr-1 DOWNTO 0);
+		  DIN:IN STD_LOGIC_VECTOR (lenght_data-1 DOWNTO 0);
+		  AP:OUT STD_LOGIC_VECTOR (lenght_addr-1 DOWNTO 0);
+		  DI:OUT STD_LOGIC_VECTOR (lenght_data-1 DOWNTO 0));
 
-end ADDR_DATA_REG ;
-
+end ADDR_DATA_REG;
 
 architecture ARCH_ADDR_DATA_REG  of  ADDR_DATA_REG  is
 
-
+	COMPONENT GEN_REGISTER
+	Generic (lenght: integer);
+	PORT(
+		D : IN std_logic_vector(lenght-1 downto 0);
+		RST : IN std_logic;
+		CE : IN std_logic;
+		CLK : IN std_logic;
+		EN : IN std_logic;          
+		Q : OUT std_logic_vector(lenght-1 downto 0)
+		);
+	END COMPONENT;
+	
+	signal en_addr, en_data : std_logic;
+	
 begin
 
--- memoria de estados --
-process (CLK, CS)
-begin
-if CS = '0' then
-	if (CLK'event and CLK ='1') then
-		if RST = '0' then 
-			DI <= (others =>'0');
-			AP <= (others =>'0');
+	Inst_REGISTER_DATA: GEN_REGISTER 
+	generic map ( 
+		lenght => lenght_data)
+	PORT MAP(
+		D => DIN,
+		RST => RST,
+		CE => CS,
+		CLK => CLK,
+		EN => en_data,
+		Q => DI
+	);
+	
+	Inst_REGISTER_ADDR: GEN_REGISTER 
+	generic map ( 
+		lenght => lenght_addr)
+	PORT MAP(
+		D => A,
+		RST => RST,
+		CE => CS,
+		CLK => CLK,
+		EN => en_addr,
+		Q => AP
+	);
+
+	process(RD,WR)
+	begin
+		if WR = '0' then
+			en_addr <= '1';
+			en_data <= '1';
+		elsif RD = '0' then
+			en_addr <= '1';
+			en_data <= '0';
 		else
-			DI <= DIN;
-			AP <= A;
+			en_addr <= '0';
+			en_data <= '0';
 		end if;
-	end if;	
-end if;	 
-end process;
+	end process;
 
 end ARCH_ADDR_DATA_REG;
 
